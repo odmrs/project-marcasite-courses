0b9549c4312a93f2f156e09d7c525d97572496b9
odmrs <omarcosviniciusdev@gmail.com>
2024-02-29 18:45:44 -0300

    feat/fix: stripe push right values, and change the status of users

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -2,50 +2,71 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
+use App\Models\Student;
 use Inertia\Inertia;
-use Stripe\Stripe;
 use Stripe\Checkout\Session;
+use Stripe\Stripe;
 
 class StripeController extends Controller
 {
-
     public function index()
     {
         return Inertia::render('Stripe/Index');
     }
+
     public function checkout()
     {
-        Stripe::setApiKey(config('services.stripe.secret'));
+        $latestStudent = Student::latest()->first();
 
-        $session = Session::create([
-            'payment_method_types' => ['card'],
-            'line_items' => [
-                [
-                    'price_data' => [
-                        'currency' => 'brl',
-                        'unit_amount' => 2000, // Valor em centavos
-                        'product_data' => [
-                            'name' => 'Curso de exemplo',
-                            'description' => 'Descrição do curso',
+        if ($latestStudent) {
+            // Get all courses that user have
+            $courses = $latestStudent->courses;
+
+            $totalPrice = 0;
+
+            foreach ($courses as $course) {
+                $totalPrice += $course->course_price;
+            }
+            // Stripe wait cents value
+            $totalPriceInCents = $totalPrice * 100;
+
+            Stripe::setApiKey(config('services.stripe.secret'));
+
+            $session = Session::create([
+                'payment_method_types' => ['card'],
+                'line_items' => [
+                    [
+                        'price_data' => [
+                            'currency' => 'brl',
+                            'unit_amount' => $totalPriceInCents,
+                            'product_data' => [
+                                'name' => 'Cursos do usuário',
+                                'description' => 'Todos os cursos associados ao usuário',
+                            ],
                         ],
+                        'quantity' => 1,
                     ],
-                    'quantity' => 1,
                 ],
-            ],
-            'mode' => 'payment',
-            'success_url' => route('stripe.success'),
-            'cancel_url' => route('stripe.cancel'),
-        ]);
+                'mode' => 'payment',
+                'success_url' => route('stripe.success'),
+                'cancel_url' => route('stripe.cancel'),
+            ]);
 
-        // Retorne um redirecionamento Inertia para a página de sucesso
-        return redirect()->route('stripe.success');
+            return redirect($session->url);
+        } else {
+            return redirect()->route('stripe.cancel');
+        }
     }
 
-    // Screen of success transition and Cancel
     public function success()
     {
+        $latestStudent = Student::latest()->first();
+
+        if ($latestStudent && $latestStudent->status === 'pendente') {
+            $latestStudent->update(['status' => 'pago']);
+        }
+
+        // Renderize a página de sucesso
         return Inertia::render('Stripe/Success');
     }
 
