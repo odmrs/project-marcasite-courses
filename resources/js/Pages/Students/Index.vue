8e7963bfcb5d7ce7ec010ef0e76b41bbb212f027
odmrs <omarcosviniciusdev@gmail.com>
2024-02-29 19:31:26 -0300

    fix: administrator can see all courses of students

--- resources/js/Pages/Students/Index.vue
+++ resources/js/Pages/Students/Index.vue
@@ -143,8 +143,17 @@ let atual_student_id;
                             v-for="student in filteredStudents"
                             :key="student.id"
                         >
-                            <td class="px-6 py-4 whitespace-nowrap">
-                                {{ student.name }}
+                            <td
+                                class="px-6 py-4 whitespace-nowrap"
+                                @click="route"
+                                style="cursor: pointer"
+                            >
+                                <InertiaLink
+                                    :href="route('courses.all')"
+                                    class="px-4 py-2 text-black font-semibold rounded-md transition duration-300 ease-in-out hover:bg-gray-100 hover:text-black text-black underline inline-block"
+                                >
+                                    {{ student.name }}</InertiaLink
+                                >
                             </td>
                             <td class="px-6 py-4 whitespace-nowrap">
                                 {{
